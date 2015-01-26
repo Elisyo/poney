49b3241a0dc666b00bfab20053570b5bdc11f724
Medhi BUSTIN <medhi.bustin@etudiant.univ-lille1.fr>
2015-01-26 15:15:32 +0100

    map

--- webserver/main.c
+++ webserver/main.c
@@ -1,17 +1,19 @@
 # include <stdio.h>
 # include <string.h>
+#include <unistd.h>
 
 int main(int argc, char ** argv){
 
-/* Arnold Robbins in the LJ of February ’95 , describing RCS */
+	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
 
-if ( argc > 1 && strcmp (argv [1] , "-advice") == 0) {
-	printf ("Don’t Panic !\n");
-	return 42;
-}
+	if ( argc > 1 && strcmp (argv [1] , "-advice") == 0) {
+		printf ("Don’t Panic !\n");
+		return 42;
+	}
 
-printf ( "Need an advice ?\n" );
+	sleep(1);
+	printf ( "Need an advice ?\n" );
 
-return 0;
+	return 0;
 
 }
