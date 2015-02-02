5a8046f6038b29afc6e8c634c05d13e16d9fbe80
Medhi BUSTIN <medhi.bustin@etudiant.univ-lille1.fr>
2015-02-02 16:18:30 +0100

    signal

--- webserver/socket.c
+++ webserver/socket.c
@@ -6,10 +6,30 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void traitement_signal(int sig){
+	int status;	
+	printf("Signal%dreçu\n",sig);
+	if (sig == SIGCHLD)
+	{
+		wait(&status);
+		if (WIFSIGNALED(status))
+			printf("Fils terminé par signal: %d\n", WTERMSIG(status));
+	}
+}
 
 void initialiser_signaux(void){
-	if (signal(SIGPIPE ,SIG_IGN) == SIG_ERR)
+	if(signal(SIGPIPE ,SIG_IGN) == SIG_ERR)
 		perror ("signal");
+	struct sigaction sa;
+	sa.sa_handler = traitement_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if(sigaction(SIGCHLD, &sa, NULL) == -1){
+		perror("sigaction(SIGCHLD)");
+	}
 }
 
 int creer_serveur (int port){
