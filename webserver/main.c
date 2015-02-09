290996bb15613246dfd087fd208b9b446d0e03dd
Medhi BUSTIN <medhi.bustin@etudiant.univ-lille1.fr>
2015-02-09 14:26:20 +0100

    fdopen

--- webserver/main.c
+++ webserver/main.c
@@ -12,33 +12,39 @@
 int connexion(int socket_serveur) {
 	int socket_client;
 	int pid;
-	char * message_recu = malloc(50);
-	const char * message_bienvenue = "Bonjour , bienvenue sur mon serveur, je vais vous chanter une petite chanson : coucou c'est moi moumou la reine des moueeeeeeeeettes qui s'en va tout droit vers euralille, fait attention, fait attention, la chanson recommence... .\nAh tchou tchou pouet pouet la voila, la totomobile, ah tchou thou pouet pouet la voila, que fait-elle donc la ! Jour mémorable de sa première sortie, touloute touloute, lorsqu'elle entra dans une confiserie, dans une confiserie ! hummmmmmmmmmm !!!!!!\nQue viens-tu faire ici?\nTu veux de l'argent ? bah t'en aura pas !\nTu veux une bonne note ? Fini ce TP !!!\nlolilol!\n#hashtag\nUn mur en brique tombe et casse ca donne quoi ? un mur cassé;\n";
+	char * message_recu = malloc(150);
+	FILE *file;
+	const char * message_bienvenue = "Bonjour , bienvenue sur mon serveur, je vais vous chanter une petite chanson : coucou c'est moi moumou la reine des moueeeeeeeeettes qui s'en va tout droit vers euralille, fait attention, fait attention, la chanson recommence... .\nAh tchou tchou pouet pouet la voila, la totomobile, ah tchou thou pouet pouet la voila, que fait-elle donc la ! Jour mémorable de sa première sortie, touloute touloute, lorsqu'elle entra dans une confiserie, dans une confiserie ! hummmmmmmmmmm !!!!!!\nQue viens-tu faire ici?\nTu veux de l'argent ? bah t'en aura pas !\nTu veux une bonne note ? Fini ce TP !!!\nlolilol!\n#hashtag\nUn mur en brique tombe et casse ca donne quoi ? un mur cassé;\n------>";
 	socket_client = accept(socket_serveur, NULL, NULL);
 	if(socket_client == -1){
 		perror("accept");
 		return -1;
-	}
+	} 
+	file = fdopen(socket_client, "w+");
 	pid = fork();
 	if(pid == -1){
 		perror("pid");
 	}else if(pid == 0){
 		/* On peut maintenant dialoguer avec le client */
-		sleep(1);
 		write(socket_client, message_bienvenue, strlen(message_bienvenue));
+		
 		while(1){
-			int taille;
+			/*int taille;
 			if((taille = read(socket_client, message_recu, 50))<=0){
 				if (taille == -1)
 					perror("read message");				
 				exit(taille != -1);
 			}
-			if(strcmp(message_recu, "") != 0){
-				if(write(socket_client, message_recu, taille)==-1){
-					perror("write message");
-					exit(1);
-				}
+			if(write(socket_client, message_recu, taille)==-1){
+				perror("write message");
+				exit(1);
+			}*/
+			if(fgets(message_recu, 150, file) == NULL){
+				perror("fgets");
+				return -1;
 			}
+			fprintf(file,"<poney>%s------>",message_recu);
+			printf("%s",message_recu);
 		}
 	} else {
 		close(socket_client);
